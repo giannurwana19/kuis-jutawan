693cca86bf9fc4fca78d8a9e457d044b201591e8
giannurwana19 <giannurwana19@gmail.com>
2021-12-11 11:10:57 +0700

    add audio answer

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,9 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import useSound from 'use-sound';
+import play from '../assets/sounds/play.mp3';
+import correct from '../assets/sounds/correct.mp3';
+import wrong from '../assets/sounds/wrong.mp3';
 
 const Trivia = ({ data, setStop, questionNumber, setQuestionNumber }) => {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState('answer');
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -21,12 +34,18 @@ const Trivia = ({ data, setStop, questionNumber, setQuestionNumber }) => {
     delay(3000, () => {
       setClassName(answer.correct ? 'answer correct' : 'answer wrong');
     });
-    delay(6000, () => {
+    delay(5000, () => {
       if (answer.correct) {
-        setQuestionNumber(prev => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber(prev => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
@@ -37,6 +56,7 @@ const Trivia = ({ data, setStop, questionNumber, setQuestionNumber }) => {
       <div className="answers">
         {question?.answers.map(answer => (
           <div
+            key={answer.text}
             className={selectedAnswer === answer ? className : 'answer'}
             onClick={() => handleClick(answer)}>
             {answer.text}
